50ae1b447df12b80e20d7892a677503a3bbfb1c1
Neofytos Zacharia <neoph.za@gmail.com>
2024-11-27 16:52:24 +0200

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+function NotFoundPage() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen p-4'>
+        <div className='text-center'>
+            <h1 className='text-6xl font-bold text-primary'>404</h1>
+            <h2 className='text-2xl font-semibold mb-4'>Page Not Found</h2>
+            <p className='text-lg text-muted-foreground mb-8 max-w-md'>The page you are looking for does not exist.</p>
+            <div className='flex flex-col sm:flex-row justify-center gap-4'>
+                <Link href={"/"} className='flex items-center justify-center px-4 py-2 bg-primary text-white rounded-md hover:bg-primary/80 transition-colors'>
+                    
+                    <ArrowLeft className='w-4 h-4 mr-2' />
+                    Go back to dashboard
+                </Link>
+            </div>
+        </div>
+
+            <footer className='mt-12 text-center text-sm text-muted-foreground'>
+                If you believe this is an error, please contact support.
+        </footer>
+
+    </div>
+  )
+}
+
+export default NotFoundPage
